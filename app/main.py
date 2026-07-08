f1d6fc2f294d50f1a4c833163d5c31c533657aea
Varun Reddy <varunyvr5856@gmail.com>
2026-07-08 22:27:05 +1000

    Addressed PR review comments

--- app/main.py
+++ app/main.py
@@ -1,7 +1,3 @@
-
-from flask import Flask, app.route("/health")
-def health():
-    return {"status": "UP"}
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+ @app.route("/login")
+ def login():
+     return jsonify({"message":"Login Successful"})
